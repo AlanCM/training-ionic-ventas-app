a4838951e197b49ca74a639cca226fdbf8cbc902
Alan Cardel <alan.cardelm@gmail.com>
2018-08-04 01:14:51 -0700

    storage and actionSheet added

--- src/pages/prospectos/prospectos.ts
+++ src/pages/prospectos/prospectos.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { Storage } from '@ionic/storage';
+import { ActionSheetController } from 'ionic-angular';
 /**
  * Generated class for the ProspectosPage page.
  *
@@ -16,31 +17,79 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class ProspectosPage {
 contact:contact;
 contacts : contact[];
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public storage: Storage,public actionSheetCtrl: ActionSheetController) {
     this.contact = new contact();
     this.contacts = [];
   }
 
   save(){
-    this.contacts.push(this.contact);
+    this.storage.set(this.contact.email.toString(),this.contact).then( obj => {this.loadContacts()});
     this.contact = new contact();
-    console.log(this.contacts);
+    //console.log(this.contacts);
+  }
+
+  loadContacts(){
+    this.contacts = [];
+    this.storage.keys().then( keySet =>{
+      // get objects
+      keySet.map(key=>{
+        this.storage.get(key).then( obj =>{
+          this.contacts.push(obj);
+        });
+      });
+  
+    });
+  }
+
+  openContactActionMenu(con : contact){
+    const actionSheet = this.actionSheetCtrl.create({
+      title: 'Prospecto: ' + con.name,
+      buttons: [
+        {
+          text: 'Eliminar',
+          role: 'destructive',
+          cssClass:'danger',
+          handler: () => {
+            console.log('Eliminar: ' + con.email);
+            this.storage.remove(con.email.toString()).then(obj => this.loadContacts());
+          }
+        },{
+          text: 'Editar',
+          handler: () => {
+            console.log('Editar:' + con.email);
+            this.contact = con;
+          }
+        },{
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+
+    actionSheet.present();
+  
   }
 
   ionViewDidLoad() {
-    console.log('Cargando');
+    this.loadContacts();
   }
 
 }
 
 class contact{
-  name : String;
-  email: String;
+
+  name    : String;
+  email   : String;
+  empresa : String;
   
   constructor(){
-    this.name = "";
-    this.email= "";
+    this.name     = "";
+    this.email    = "";
+    this.empresa  = "";
   }
 
-  }
+}
 
